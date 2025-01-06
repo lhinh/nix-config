190222c40cb19809c773d918ebde9afcb20e024d
Loc <lhinh3431@gmail.com>
2025-01-05 18:25:17 -0800

    added lyr00 configs
    added unstable urls

--- flake.nix
+++ flake.nix
@@ -2,13 +2,35 @@
   description = "slippy's NixOS configuration";
 
   inputs = {
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs";
+    home-manager-unstable.url = "github:nix-community/home-manager";
+    home-manager-unstable.inputs.nixpkgs.follows = "nixpkgs";
+
     nixpkgs-2405.url = "github:NixOS/nixpkgs/nixos-24.05";
     home-manager-2405.url = "github:nix-community/home-manager/release-24.05";
     home-manager-2405.inputs.nixpkgs.follows = "nixpkgs-2405";
+
+    nixpkgs-2411.url = "github:NixOS/nixpkgs/nixos-24.11";
+    home-manager-2411.url = "github:nix-community/home-manager/release-24.11";
+    home-manager-2411.inputs.nixpkgs.follows = "nixpkgs-2411";
   };
 
   outputs = inputs@{ self, nixpkgs, ... }: {
     nixosConfigurations = {
+      lyr00 = inputs.nixpkgs-2411.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = inputs;
+        modules = [
+          ./hosts/lyr00
+
+          (import "${inputs.home-manager-2411}/nixos")
+          (import ./home-manager/lyr00.nix (stateVersion = "24.11";))
+
+          ./modules/base
+          ./modules/firefox
+        ];
+      };
+
       vmslippy = inputs.nixpkgs-2405.lib.nixosSystem {
         system = "x86_64-linux";
         specialArgs = inputs;
@@ -16,7 +38,7 @@
           ./hosts/vmslippy
 
           (import "${inputs.home-manager-2405}/nixos")
-          ./home-manager
+          (import ./home-manager (stateVersion = "24.05";))
 
           ./modules/base
           ./modules/firefox
---
+++ hosts/lyr00/default.nix
@@ -0,0 +1,33 @@
+{ config, pkgs, ... }:
+{
+  imports =
+    [
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/sda";
+  boot.loader.grub.useOSProber = true;
+
+  networking.hostName = "lyr00";
+
+  # Install VMtools
+  virtualisation.vmware.guest.enable = true;
+
+  # Enable automatic login for the user.
+  services.displayManager.autoLogin.enable = true;
+  services.displayManager.autoLogin.user = "slippy";
+
+  # Set default editor to vim.
+  environment.variables.EDITOR = "vim";
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.11"; # Did you read the comment?
+
+}
