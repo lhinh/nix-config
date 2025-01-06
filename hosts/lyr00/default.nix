bcfa8922dcd9f301ab474d3b53610d7706d9ee31
Loc Hinh <lhinh3431@gmail.com>
2025-01-06 11:36:54 -0800

    using systemd-boot instead of GRUB

--- hosts/lyr00/default.nix
+++ hosts/lyr00/default.nix
@@ -6,9 +6,8 @@
     ];
 
   # Bootloader.
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dev/sda";
-  boot.loader.grub.useOSProber = true;
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "lyr00";
 
